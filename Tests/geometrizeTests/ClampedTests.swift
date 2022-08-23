10b4bee82285b512cf26a9f877ec8538ec074ae7
Valeriy Van <github@w7software.com>
2022-08-23 16:03:27 +0300

    Add test for clamped function

---
+++ Tests/geometrizeTests/ClampedTests.swift
@@ -0,0 +1,13 @@
+import XCTest
+@testable import geometrize
+
+final class ClampedTests: XCTestCase {
+    
+    func test() throws {
+        XCTAssertEqual(5.clamped(to: 5...10), 5)
+        XCTAssertEqual(3.clamped(to: 5...10), 5)
+        XCTAssertEqual(10.clamped(to: 5...10), 10)
+        XCTAssertEqual(20.clamped(to: 5...10), 10)
+    }
+    
+}
