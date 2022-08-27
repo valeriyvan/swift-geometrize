dc8dd1bccbdaebe9c9440cc81008245b672aab04
Valeriy Van <github@w7software.com>
2022-08-27 07:34:15 +0300

    Change SVG header to make Finder and Safari open generated SVGs.

--- Sources/geometrize/SVGExporter.swift
+++ Sources/geometrize/SVGExporter.swift
@@ -55,8 +55,9 @@ func getSingleShapeSVGData(color: Rgba, shape: any Shape, options: SVGExportOpti
 // @return A string representing the SVG image.
 func exportSingleShapeSVG(color: Rgba, shape: any Shape, width: Int, height: Int, options: SVGExportOptions = SVGExportOptions()) -> String {
     """
-    <?xml version=\"1.0\" standalone=\"no\"?>
-    <svg xmlns=\"https://www.w3.org/2000/svg\" version=\"1.2\" baseProfile=\"tiny\" width=\"\(width)\" height=\"(height)\" viewBox=\"0 0 \(width) \(height)\">\n
+    <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+    <svg xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xmlns="http://www.w3.org/2000/svg" width=\"\(width)\" height=\"\(height)\" viewBox=\"0 0 \(width) \(height)\">
     \(getSingleShapeSVGData(color: color, shape: shape, options: options))
     </svg>
     """
@@ -70,10 +71,10 @@ func exportSingleShapeSVG(color: Rgba, shape: any Shape, width: Int, height: Int
 // @return A string representing the SVG image.
 func exportSVG(data: [ShapeResult], width: Int, height: Int, options: SVGExportOptions = SVGExportOptions()) -> String {
     var str = """
-    <?xml version=\"1.0\" standalone=\"no\"?>
-    <svg xmlns=\"https://www.w3.org/2000/svg\" version=\"1.2\" baseProfile=\"tiny\" width=\"\(width)\" height=\"\(height)\" viewBox=\"0 0 \(width) \(height)\">
+    <?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+    <svg xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xmlns="http://www.w3.org/2000/svg" width=\"\(width)\" height=\"\(height)\" viewBox=\"0 0 \(width) \(height)\">\n
     """
-
     var options = options
     for (number, shapeResult) in data.enumerated() {
         options.itemId = number
