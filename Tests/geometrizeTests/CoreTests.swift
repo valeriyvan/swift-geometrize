ce31f65b5f4fee85856982d4d71b8f319410aad8
Valeriy Van <github@w7software.com>
2022-08-30 09:31:56 +0300

    Improve test

--- Tests/geometrizeTests/CoreTests.swift
+++ Tests/geometrizeTests/CoreTests.swift
@@ -9,13 +9,28 @@ final class CoreTests: XCTestCase {
         // Difference with itself is 0
         XCTAssertEqual(differenceFull(first: blackBitmap, second: blackBitmap), 0)
 
-        var bitmapOnePixelChanged = blackBitmap
-        bitmapOnePixelChanged[0, 0] = .white
-        var bitmapTwoPixelsChanged = bitmapOnePixelChanged
-        bitmapTwoPixelsChanged[0, 1] = .white
+        var blackBitmapOnePixelChanged = blackBitmap
+        blackBitmapOnePixelChanged[0, 0] = .white
+        var blackBitmapTwoPixelsChanged = blackBitmapOnePixelChanged
+        blackBitmapTwoPixelsChanged[0, 1] = .white
         
         // Changing two pixels means there's more difference than changing one.
-        XCTAssertTrue(differenceFull(first: blackBitmap, second: bitmapTwoPixelsChanged) > differenceFull(first: blackBitmap, second: bitmapOnePixelChanged))
+        XCTAssertTrue(differenceFull(first: blackBitmap, second: blackBitmapTwoPixelsChanged) > differenceFull(first: blackBitmap, second: blackBitmapOnePixelChanged))
+
+        // Now the same for white image
+        
+        let whiteBitmap = Bitmap(width: 10, height: 10, color: .white)
+
+        // Difference with itself is 0
+        XCTAssertEqual(differenceFull(first: whiteBitmap, second: whiteBitmap), 0)
+
+        var whiteBitmapOnePixelChanged = whiteBitmap
+        whiteBitmapOnePixelChanged[0, 0] = .black
+        var whiteBitmapTwoPixelsChanged = whiteBitmapOnePixelChanged
+        whiteBitmapTwoPixelsChanged[0, 1] = .black
+        
+        // Changing two pixels means there's more difference than changing one.
+        XCTAssertTrue(differenceFull(first: whiteBitmap, second: whiteBitmapTwoPixelsChanged) > differenceFull(first: whiteBitmap, second: whiteBitmapOnePixelChanged))
     }
 
 }
