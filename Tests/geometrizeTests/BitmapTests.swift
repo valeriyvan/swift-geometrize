1d825eed63d15c7ece3643ae6866d885e85568de
Valeriy Van <github@w7software.com>
2022-08-30 12:05:08 +0300

    Conform Bitmap to ExpressibleByStringLiteral

--- Tests/geometrizeTests/BitmapTests.swift
+++ Tests/geometrizeTests/BitmapTests.swift
@@ -7,7 +7,7 @@ final class BitmapTests: XCTestCase {
         let bitmap = Bitmap()
         XCTAssertEqual(bitmap.width, 0)
         XCTAssertEqual(bitmap.height, 0)
-        XCTAssertEqual(bitmap.data, [])
+        XCTAssertEqual(bitmap.backing, [])
         XCTAssertTrue(bitmap.isEmpty)
     }
 
@@ -15,15 +15,15 @@ final class BitmapTests: XCTestCase {
         let blackBitmap = Bitmap(width: 5, height: 7, color: Rgba(r: 0, g: 0, b: 0, a: 0))
         XCTAssertEqual(blackBitmap.width, 5)
         XCTAssertEqual(blackBitmap.height, 7)
-        XCTAssertEqual(blackBitmap.data.count, 5 * 7 * 4)
-        XCTAssertEqual(blackBitmap.data, [UInt8].init(repeating: 0, count: 5 * 7 * 4))
+        XCTAssertEqual(blackBitmap.backing.count, 5 * 7 * 4)
+        XCTAssertEqual(blackBitmap.backing, ContiguousArray<UInt8>(repeating: 0, count: 5 * 7 * 4))
         XCTAssertFalse(blackBitmap.isEmpty)
 
         let whiteBitmap = Bitmap(width: 8, height: 6, color: .white)
         XCTAssertEqual(whiteBitmap.width, 8)
         XCTAssertEqual(whiteBitmap.height, 6)
-        XCTAssertEqual(whiteBitmap.data.count, 8 * 6 * 4)
-        XCTAssertEqual(whiteBitmap.data, [UInt8].init(repeating: 255, count: 8 * 6 * 4))
+        XCTAssertEqual(whiteBitmap.backing.count, 8 * 6 * 4)
+        XCTAssertEqual(whiteBitmap.backing, ContiguousArray<UInt8>(repeating: 255, count: 8 * 6 * 4))
         XCTAssertFalse(whiteBitmap.isEmpty)
     }
 
@@ -36,8 +36,8 @@ final class BitmapTests: XCTestCase {
         let bitmap = Bitmap(width: 5, height: 3, data: data)
         XCTAssertEqual(bitmap.width, 5)
         XCTAssertEqual(bitmap.height, 3)
-        XCTAssertEqual(bitmap.data.count, 5 * 3 * 4)
-        XCTAssertEqual(bitmap.data, data)
+        XCTAssertEqual(bitmap.backing.count, 5 * 3 * 4)
+        XCTAssertEqual(bitmap.backing, ContiguousArray<UInt8>(data))
     }
 
     func testSubscript() throws {
@@ -53,11 +53,11 @@ final class BitmapTests: XCTestCase {
         bitmap[1, 1] = Rgba(r: 111, g: 111, b: 111, a: 111)
         bitmap[3, 2] = Rgba(r: 222, g: 222, b: 222, a: 222)
         XCTAssertEqual(
-            bitmap.data, [
+            bitmap.backing, [
                 0,0,0,0,     1,1,1,1,         2,2,2,2,     3,3,3,3,         4,4,4,4,
                 10,10,10,10, 111,111,111,111, 12,12,12,12, 13,13,13,13,     14,14,14,14,
                 20,20,20,20, 21,21,21,21,     22,22,22,22, 222,222,222,222, 24,24,24,24
-            ] as [UInt8]
+            ] as ContiguousArray<UInt8>
         )
     }
 
@@ -97,6 +97,23 @@ final class BitmapTests: XCTestCase {
                 XCTAssertEqual(bitmap[x, y], color)
             }
         }
+    }
+    
+    func testInitFromString() {
+        XCTAssertEqual(
+            Bitmap(
+                stringLiteral:
+                """
+                width: 3, height: 2
+                1,2,3,4,5,6,7,8,9,10,11,12,
+                13,14,15,16,17,18,19,20,21,22,23,24
+                """
+            ),
+            Bitmap(
+                width: 3, height: 2,
+                data: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
+            )
+        )
     }
     
 }
