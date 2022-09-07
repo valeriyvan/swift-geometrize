8bc8dde510799fe7768f24e4f92b88fd39d29937
Valeriy Van <github@w7software.com>
2022-09-07 10:51:47 +0300

    Minor refactoring

--- Sources/geometrize/Shapes/Rectangle.swift
+++ Sources/geometrize/Shapes/Rectangle.swift
@@ -19,8 +19,7 @@ final class Rectangle: Shape {
     }
 
     func copy() -> Rectangle {
-        let aCopy = Rectangle(x1: x1, y1: y1, x2: x2, y2: y2)
-        return aCopy
+        Rectangle(x1: x1, y1: y1, x2: x2, y2: y2)
     }
     
     func setup(xMin: Int, yMin: Int, xMax: Int, yMax: Int) {
