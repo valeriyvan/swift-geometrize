2917ba6866375dc5b81ade10059e331dff7e0643
Valeriy Van <github@w7software.com>
2022-09-13 12:25:52 +0300

    Refactor Point. Conform Point to Hashable and CustomStringConvertible.

--- Sources/geometrize/Point.swift
+++ Sources/geometrize/Point.swift
@@ -1,12 +1,27 @@
 import Foundation
 
-struct Point<SignedInteger> {
-    var x, y: SignedInteger
+public struct Point<N: Numeric> {
+    var x, y: N
+    
+    init(x: N, y: N) {
+        self.x = x
+        self.y = y
+    }
 }
 
 extension Point<Int> {
     init(_ p: Point<Double>) {
         x = Int(p.x)
         y = Int(p.y)
+    }
+}
+
+extension Point: Equatable where N: Equatable {}
+
+extension Point: Hashable where N: Hashable {}
+
+extension Point: CustomStringConvertible where N: CustomStringConvertible {
+    public var description: String {
+        "Point(x=\(x), y=\(y))"
     }
 }
