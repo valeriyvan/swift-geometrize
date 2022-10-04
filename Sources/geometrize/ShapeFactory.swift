67cb928c468ec00638082117b444bc1de0a01127
Valeriy Van <github@w7software.com>
2022-10-04 11:08:36 +0300

    Linting

--- Sources/geometrize/ShapeFactory.swift
+++ Sources/geometrize/ShapeFactory.swift
@@ -1,13 +1,14 @@
 import Foundation
 
-// Creates an instance of the default shape creator object.
-// The setup, mutate and rasterize methods are bound with default methods.
-// @param types The types of shapes to create.
-// @param xMin The minimum x coordinate of the shapes created.
-// @param yMin The minimum y coordinate of the shapes created.
-// @param xMax The maximum x coordinate of the shapes created.
-// @param yMax The maximum y coordinate of the shapes created.
-// @return The default shape creator.
+/// Creates an instance of the default shape creator object.
+/// The setup, mutate and rasterize methods are bound with default methods.
+/// - Parameters:
+///   - types: The types of shapes to create.
+///   - xMin: The minimum x coordinate of the shapes created.
+///   - yMin: The minimum y coordinate of the shapes created.
+///   - xMax: The maximum x coordinate of the shapes created.
+///   - yMax: The maximum y coordinate of the shapes created.
+/// - Returns: The default shape creator.
 public func createDefaultShapeCreator(types: Set<ShapeType>, xMin: Int, yMin: Int, xMax: Int, yMax: Int) -> () -> any Shape {
     canvasBounds = (xMin: xMin, yMin: yMin, xMax: xMax, yMax: yMax)
     return {
