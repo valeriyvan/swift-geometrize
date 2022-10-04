76e315844677573f233b48aac66a2a1e64f26970
Valeriy Van <github@w7software.com>
2022-10-04 11:09:13 +0300

    Linting

--- Sources/geometrize/ShapeResult.swift
+++ Sources/geometrize/ShapeResult.swift
@@ -1,11 +1,11 @@
 import Foundation
 
- // Container for info about a shape added to the model.
+ /// Container for info about a shape added to the model.
 public struct ShapeResult {
     public let score: Double
     public let color: Rgba
     public let shape: any Shape
-    
+
     public init(score: Double, color: Rgba, shape: any Shape) {
         self.score = score
         self.color = color
