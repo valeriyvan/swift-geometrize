821aec6354243992e5a623081afa63537f85206f
Valeriy Van <github@w7software.com>
2022-10-04 09:39:14 +0300

    Linting

--- Tests/geometrizeTests/bresenhamTests.swift
+++ Tests/geometrizeTests/bresenhamTests.swift
@@ -4,7 +4,7 @@ import SnapshotTesting
 
 @testable import geometrize
 
-final class bresenhamTests: XCTestCase {
+final class BresenhamTests: XCTestCase {
 
     func testBresenham() throws {
         var bitmap = Bitmap(width: 500, height: 400, color: .red.withAlphaComponent(128))
@@ -16,7 +16,7 @@ final class bresenhamTests: XCTestCase {
         points.append(contentsOf: bresenham(from: Point(x: 475, y: 321), to: Point(x: 474, y: 376)))
         points.append(contentsOf: bresenham(from: Point(x: 474, y: 376), to: Point(x: 50, y: 350)))
         points.append(contentsOf: bresenham(from: Point(x: 50, y: 350), to: Point(x: 50, y: 50)))
-        points.forEach{ bitmap[$0] = .black }
+        points.forEach { bitmap[$0] = .black }
         bitmap.draw(lines: scaleScanlinesTrimmed(width: 500, height: 400, step: 100), color: .black)
         assertSnapshot(matching: bitmap, as: SimplySnapshotting(pathExtension: "png", diffing: Diffing<Bitmap>.image))
     }
