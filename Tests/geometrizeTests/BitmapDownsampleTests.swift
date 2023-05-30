0590dad34adf3883519ddaf191556303de47c972
Valeriy Van <github@w7software.com>
2023-05-30 19:52:19 +0300

    Fix compile error in test

--- Tests/geometrizeTests/BitmapDownsampleTests.swift
+++ Tests/geometrizeTests/BitmapDownsampleTests.swift
@@ -6,18 +6,19 @@ final class BitmapDownsampleTests: XCTestCase {
 
     func testBitmapDownsample() throws {
         let width = 500, height = 500
+        let canvasBoundsProvider = { Bounds(xMin: 0, xMax: width, yMin: 0, yMax: height) }
         let xMax = width - 1, yMax = width - 1
         var bitmap = Bitmap(width: width, height: height, color: .white)
         bitmap.draw(
             lines:
-                Ellipse(x: 250.0, y: 250.0, rx: 245.0, ry: 100.0)
+                Ellipse(canvasBoundsProvider: canvasBoundsProvider, x: 250.0, y: 250.0, rx: 245.0, ry: 100.0)
                 .rasterize(xMin: 0, yMin: 0, xMax: xMax, yMax: yMax),
             color:
                 .red.withAlphaComponent(200)
         )
         bitmap.draw(
             lines:
-                Ellipse(x: 250.0, y: 250.0, rx: 100.0, ry: 245.0)
+                Ellipse(canvasBoundsProvider: canvasBoundsProvider, x: 250.0, y: 250.0, rx: 100.0, ry: 245.0)
                 .rasterize(xMin: 0, yMin: 0, xMax: xMax, yMax: yMax),
             color:
                 .green.withAlphaComponent(200)
