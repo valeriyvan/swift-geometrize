29a60b3c8457acf64af5b4c8525458b91af66338
Valeriy Van <github@w7software.com>
2023-12-14 00:45:11 +0200

    Add transpose

--- Tests/geometrizeTests/BitmapTests.swift
+++ Tests/geometrizeTests/BitmapTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import SnapshotTesting
 @testable import Geometrize
 
 final class BitmapTests: XCTestCase {
@@ -145,6 +146,34 @@ final class BitmapTests: XCTestCase {
                 XCTAssertEqual(bitmap[x, y], color)
             }
         }
+    }
+
+    func testTranspose() {
+        let width = 500, height = 500
+        let xRange = 0...width - 1, yRange = 0...height - 1
+        var bitmap = Bitmap(width: width, height: height, color: .white)
+        bitmap.draw(
+            lines:
+                Ellipse(strokeWidth: 1, x: 250.0, y: 250.0, rx: 245.0, ry: 100.0)
+                .rasterize(x: xRange, y: yRange),
+            color:
+                .red.withAlphaComponent(200)
+        )
+        bitmap.draw(
+            lines:
+                Ellipse(strokeWidth: 1, x: 250.0, y: 250.0, rx: 100.0, ry: 245.0)
+                .rasterize(x: xRange, y: yRange),
+            color:
+                .green.withAlphaComponent(200)
+        )
+        bitmap.draw(lines: scaleScanlinesTrimmed(width: width, height: height, step: 100), color: .black)
+
+        bitmap.transpose()
+
+        assertSnapshot(
+            matching: bitmap,
+            as: SimplySnapshotting(pathExtension: "png", diffing: Diffing<Bitmap>.image)
+        )
     }
 
     func testDraw() {
