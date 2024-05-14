c3c94b31d2eb9ba32817747a634f47e837396609
Valeriy Van <github@w7software.com>
2024-05-14 09:59:14 +0300

    Replace Bitmap(stringBundleResource:extension) with Bitmap(ppmBundleResource:extension)

--- Tests/geometrizeTests/Bitmap+ppmBundleResource.swift
+++ Tests/geometrizeTests/Bitmap+ppmBundleResource.swift
@@ -1,14 +1,14 @@
-import Foundation
+import XCTest
 import Geometrize
 
 extension Bitmap {
 
-    init(stringBundleResource resource: String, withExtension extension: String) throws {
+    init(ppmBundleResource resource: String, withExtension extension: String) throws {
         guard let url = Bundle.module.url(forResource: resource, withExtension: `extension`) else {
             fatalError("Resource \"\(resource).\(`extension`)\" not found in bundle")
         }
         let string = try String(contentsOf: url)
-        self = Bitmap(stringLiteral: string)
+        self = try Bitmap(ppmString: string)
     }
 
 }
