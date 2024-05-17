13d0b27dfbc23ff3f4f1d272872024cce8a14db6
Valeriy Van <github@w7software.com>
2024-05-18 00:01:10 +0300

    Fix test

--- Tests/geometrizeTests/ImageRunnerTests.swift
+++ Tests/geometrizeTests/ImageRunnerTests.swift
@@ -71,7 +71,7 @@ final class ImageRunnerTests: XCTestCase {
             <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
             <svg xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xmlns="http://www.w3.org/2000/svg"
                 width="100" height="100" viewBox="0 0 100 100">
-            <rect x="0.0" y="0.0" width="100.0" height="100.0" id="0" fill="rgb(255,0,0)" fill-opacity="1.0"/>
+            <rect x="0.0" y="0.0" width="100.0" height="100.0" fill="rgb(255,0,0)" fill-opacity="1.0"/>
             </svg>
             """
         )
