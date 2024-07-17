a497d17d6d8ca29e6d0ebd55d1cd8ed174c7dc11
Valeriy Van <github@w7software.com>
2024-07-17 12:32:36 +0300

    Fix minor linter issues

--- Sources/bitmapImportExport/JPEG.Data.Rectangular.swift
+++ Sources/bitmapImportExport/JPEG.Data.Rectangular.swift
@@ -7,7 +7,7 @@ extension JPEG.Data.Rectangular<JPEG.Common> {
             switch m {
             case .exif(let exif):
                 if let orientationTag = exif[tag: 0x112] {
-                    let orientation = orientationTag.box.endianness == .littleEndian 
+                    let orientation = orientationTag.box.endianness == .littleEndian
                         ? orientationTag.box.contents.0 : orientationTag.box.contents.1
                     return Int(orientation)
                 }
