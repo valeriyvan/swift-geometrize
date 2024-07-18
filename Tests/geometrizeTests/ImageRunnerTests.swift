efa8255e3b948b44a9fb55b97ebd84f77a4714f9
Valeriy Van <github@w7software.com>
2024-07-18 10:07:56 +0300

    Minor fixes in SVGExporter and tests

--- Tests/geometrizeTests/ImageRunnerTests.swift
+++ Tests/geometrizeTests/ImageRunnerTests.swift
@@ -70,7 +70,7 @@ final class ImageRunnerTests: XCTestCase {
             <?xml version="1.0" encoding="UTF-8" standalone="no"?>
             <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
             <svg xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" xmlns="http://www.w3.org/2000/svg"
-                width="100" height="100" viewBox="0 0 100 100">
+            width="100" height="100" viewBox="0 0 100 100">
             <rect x="0.0" y="0.0" width="100.0" height="100.0" fill="rgb(255,0,0)" fill-opacity="1.0"/>
             </svg>
             """
