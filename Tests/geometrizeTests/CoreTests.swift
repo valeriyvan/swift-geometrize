f83126279cfb7a5f837f4cc4ee4171763401d0ec
Valeriy Van <github@w7software.com>
2024-07-19 15:13:59 +0300

    Move test for defaultEnergyFunction into separate file

--- Tests/geometrizeTests/CoreTests.swift
+++ Tests/geometrizeTests/CoreTests.swift
@@ -3,44 +3,6 @@ import XCTest
 
 final class CoreTests: XCTestCase {
 
-    func testDefaultEnergyFunctionComparingResultWithCPlusPlus() throws {
-        let scanlines = try [Scanline](
-            stringBundleResource: "defaultEnergyFunction scanlines",
-            withExtension: "txt"
-        )
-        let bitmapTarget = try Bitmap(
-            ppmBundleResource: "defaultEnergyFunction target bitmap",
-            withExtension: "ppm"
-        )
-        let bitmapCurrent = try Bitmap(
-            ppmBundleResource: "defaultEnergyFunction current bitmap",
-            withExtension: "ppm"
-        )
-        var bitmapBuffer = try Bitmap(
-            ppmBundleResource: "defaultEnergyFunction buffer bitmap",
-            withExtension: "ppm"
-        )
-        let bitmapBufferOnExit = try Bitmap(
-            ppmBundleResource: "defaultEnergyFunction buffer bitmap on exit",
-            withExtension: "ppm"
-        )
-
-        XCTAssertEqual(
-            defaultEnergyFunction(
-                scanlines,
-                128 /* alpha */,
-                bitmapTarget,
-                bitmapCurrent,
-                &bitmapBuffer,
-                0.162824
-            ),
-            0.162776,
-            accuracy: 0.000001
-        )
-
-        XCTAssertEqual(bitmapBuffer, bitmapBufferOnExit)
-    }
-
     // fails
     func testHillClimbComparingResultWithCPlusPlus() throws { // swiftlint:disable:this function_body_length
         let url = Bundle.module.url(
