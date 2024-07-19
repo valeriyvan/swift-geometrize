19b825292b30addfc66a1a71794af614fc7c6477
Valeriy Van <github@w7software.com>
2024-07-19 15:09:34 +0300

    Move bitmap tests from CoreTests to BitmapTests

--- Tests/geometrizeTests/CoreTests.swift
+++ Tests/geometrizeTests/CoreTests.swift
@@ -3,64 +3,6 @@ import XCTest
 
 final class CoreTests: XCTestCase {
 
-    func testDifferenceFull() throws {
-        let blackBitmap = Bitmap(width: 10, height: 10, color: .black)
-
-        // Difference with itself is 0
-        XCTAssertEqual(blackBitmap.differenceFull(with: blackBitmap), 0)
-
-        var blackBitmapOnePixelChanged = blackBitmap
-        blackBitmapOnePixelChanged[0, 0] = .white
-        var blackBitmapTwoPixelsChanged = blackBitmapOnePixelChanged
-        blackBitmapTwoPixelsChanged[0, 1] = .white
-
-        // Changing two pixels means there's more difference than changing one.
-        XCTAssertGreaterThan(
-            blackBitmap.differenceFull(with: blackBitmapTwoPixelsChanged),
-            blackBitmap.differenceFull(with: blackBitmapOnePixelChanged)
-        )
-
-        // Now the same for white image
-        let whiteBitmap = Bitmap(width: 10, height: 10, color: .white)
-
-        // Difference with itself is 0
-        XCTAssertEqual(whiteBitmap.differenceFull(with: whiteBitmap), 0)
-
-        var whiteBitmapOnePixelChanged = whiteBitmap
-        whiteBitmapOnePixelChanged[0, 0] = .black
-        var whiteBitmapTwoPixelsChanged = whiteBitmapOnePixelChanged
-        whiteBitmapTwoPixelsChanged[0, 1] = .black
-
-        // Changing two pixels means there's more difference than changing one.
-        XCTAssertGreaterThan(
-            whiteBitmap.differenceFull(with: whiteBitmapTwoPixelsChanged),
-            whiteBitmap.differenceFull(with: whiteBitmapOnePixelChanged)
-        )
-    }
-
-    func testDifferenceFullComparingResultWithCPlusPlus() throws {
-        let bitmapFirst = try Bitmap(ppmBundleResource: "differenceFull bitmap first", withExtension: "ppm")
-        let bitmapSecond = try Bitmap(ppmBundleResource: "differenceFull bitmap second", withExtension: "ppm")
-        XCTAssertEqual(bitmapFirst.differenceFull(with: bitmapSecond), 0.170819, accuracy: 0.000001)
-    }
-
-    func testDifferencePartialComparingResultWithCPlusPlus() throws {
-        let bitmapTarget = try Bitmap(ppmBundleResource: "differencePartial bitmap target", withExtension: "ppm")
-        let bitmapBefore = try Bitmap(ppmBundleResource: "differencePartial bitmap before", withExtension: "ppm")
-        let bitmapAfter = try Bitmap(ppmBundleResource: "differencePartial bitmap after", withExtension: "ppm")
-        let scanlines = try [Scanline](stringBundleResource: "differencePartial scanlines", withExtension: "txt")
-        XCTAssertEqual(
-            bitmapBefore.differencePartial(
-                with: bitmapAfter,
-                target: bitmapTarget,
-                score: 0.170819,
-                mask: scanlines
-            ),
-            0.170800,
-            accuracy: 0.000001
-        )
-    }
-
     func testDefaultEnergyFunctionComparingResultWithCPlusPlus() throws {
         let scanlines = try [Scanline](
             stringBundleResource: "defaultEnergyFunction scanlines",
