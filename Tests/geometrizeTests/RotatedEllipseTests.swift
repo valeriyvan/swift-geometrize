60729857e4caf0c8e10a1a918071121b66a098f3
Valeriy Van <github@w7software.com>
2025-07-28 23:06:03 +0300

    Add tests for RotatedEllipse

---
+++ Tests/geometrizeTests/RotatedEllipseTests.swift
@@ -0,0 +1,32 @@
+import XCTest
+import SnapshotTesting
+@testable import Geometrize
+
+final class RotatedEllipseTests: XCTestCase {
+
+    func testRasterize() throws {
+        let width = 500, height = 500
+        let xRange = 0...width - 1, yRange = 0...height - 1
+        var bitmap = Bitmap(width: width, height: height, color: .white)
+        bitmap.draw(
+            lines:
+                RotatedEllipse(strokeWidth: 1, x: 250.0, y: 250.0, rx: 245.0, ry: 100.0, angleDegrees: 30.0)
+                .rasterize(x: xRange, y: yRange),
+            color:
+                .red.withAlphaComponent(200)
+        )
+        bitmap.draw(
+            lines:
+                RotatedEllipse(strokeWidth: 1, x: 250.0, y: 250.0, rx: 100.0, ry: 245.0, angleDegrees: 60.0)
+                .rasterize(x: xRange, y: yRange),
+            color:
+                .green.withAlphaComponent(200)
+        )
+        bitmap.draw(lines: scaleScanlinesTrimmed(width: width, height: height, step: 100), color: .black)
+        assertSnapshot(
+            of: bitmap,
+            as: SimplySnapshotting(pathExtension: "png", diffing: Diffing<Bitmap>.image)
+        )
+    }
+
+}
